4bba49d23d9fbfa4792236758cc5f20ae246b782
unknown <alexey.markov@bostongene.com>
2018-04-08 23:54:56 +0300

    Добавил выбор 1-man-squad, добавил распознование состояний. К сожалению работает только на мониторах - 1440x900

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ from utils import *
 print("Initializing bot...")
 sleep(5)
 while(True):
-    start_pubg_game()
-    sleep(3)
-    press_button("f")
-    sleep(2)
-    exit_to_lobby()
-    sleep(1)
-    press_reconnect()
+    print(pyautogui.position())
+    image = pyautogui.screenshot()
+    if (is_in_airplane(image)):
+        print("I'm in airplane")
+        sleep(40)
+        press_button("f")
+        sleep(90)
+        walk_forward()
+        sleep(190)
+        leave_match()
+    if (is_in_main_menu(image)):
+        print("I'm in main menu")
+        start_pubg_game()
+        sleep(25)
+    if (in_kill_screen(image)):
+        print("I'm in kill screen")
+        exit_to_lobby()
+        sleep(10)
+    if (in_reconnect_screen(image)):
+        print("I'm in recconnect screen")
+        press_reconnect()
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from time import sleep
 screen_width, screen_height = pyautogui.size()
 
 def start_pubg_game():
+    move_mouse_to(131, 660)
+    press_LMB()
     move_mouse_to(screen_width * 0.1, screen_height * 0.95)
     press_LMB()
 
@@ -15,6 +17,44 @@ def exit_to_lobby():
 
 def press_reconnect():
     move_mouse_to(screen_width * 0.5, screen_height * 0.55)
+    press_LMB()
+    move_mouse_to(screen_width * 0.5, screen_height * 0.6)
+    press_LMB()
+    move_mouse_to(screen_width * 0.5, screen_height * 0.65)
+    press_LMB()
+
+def walk_forward():
+    press_button("numlock")  
+
+def is_in_airplane(image):
+    point_one = image.getpixel((screen_width * 0.07, screen_height * 0.85)) == (255, 216, 0)
+    point_two = image.getpixel((99, 710)) == (255, 216, 0)
+    point_three = image.getpixel((87, 770)) == (255, 216, 0)
+    point_four = image.getpixel((103, 797)) == (255, 216, 0)
+    first = image.getpixel((screen_width * 0.07, screen_height * 0.85)) == (255, 216, 0)
+    return point_one or point_two or point_three or point_four
+
+def is_in_main_menu(image):
+    return(image.getpixel((177, 40))) == (255, 199, 0)
+
+def in_kill_screen(image):
+    return(image.getpixel((81, 127))) == (248, 200, 0)
+
+def in_reconnect_screen(image):
+    return(image.getpixel((186, 824))) == (245, 203, 41)
+
+def lay_down():
+    pyautogui.press("z")
+
+def leave_match():
+    press_button('escape')
+    sleep(1)
+    move_mouse_to(714, 500)
+    sleep(1)
+    press_LMB()
+    sleep(1)
+    move_mouse_to(615, 475)
+    sleep(1)
     press_LMB()
 
 def press_button(button):
